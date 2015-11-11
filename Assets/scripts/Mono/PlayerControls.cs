ba02950774cc91aae48bfbb20b1a05fab5ad6234
misaherianova <miejja@gmail.com>
2015-11-11 18:09:58 +0100

    Pokus_commit1.0

--- Assets/scripts/Mono/PlayerControls.cs
+++ Assets/scripts/Mono/PlayerControls.cs
@@ -4,7 +4,7 @@ namespace Assets.scripts.Mono
 {
 	public class PlayerControls : MonoBehaviour
 	{
-		public GameObject body;
+		public GameObject body; // kvuli pokusu o commit ..
 
 		public PlayerData data;
 		public PlayerUI ui;
