b04c9080c5c9cdf487edc53800357c164ce68b0b
lukashonke <honke.lukas@gmail.com>
2016-01-20 14:32:57 +0100

    sync

--- Assets/scripts/Mono/MapGenerator/Levels/TestLevelData.cs
+++ Assets/scripts/Mono/MapGenerator/Levels/TestLevelData.cs
@@ -16,14 +16,18 @@ namespace Assets.scripts.Mono.MapGenerator.Levels
 
 		public override void Generate()
 		{
-			map.GenerateDungeonRegion(0, 0, 35, true, new[] { 344 }); // 0 0
-			map.GenerateDungeonRegion(0, 1, 35, false, true, false, new[] { 344 }, 1, 2); // 0, 2
+			map.GenerateDungeonRegion(0, 0, WorldHolder.instance.randomFillPercent, true, null); // 0 0
+			map.GenerateDungeonRegion(0, 1, WorldHolder.instance.randomFillPercent, false, true, false, null, 1, 2); // 0, 2
 
-			map.GenerateDungeonRegion(1, 1, WorldHolder.instance.randomFillPercent, false, true, true, new[] { 290 }, 2, 1); // 0 1; 1 1
-			map.GenerateDungeonRegion(1, 2, WorldHolder.instance.randomFillPercent, false, true, true, new[] { 290 }, 2, 1); // 0 1; 1 1
+			map.GenerateDungeonRegion(1, 1, WorldHolder.instance.randomFillPercent, false, true, true, null, 2, 1); // 0 1; 1 1
+			map.GenerateDungeonRegion(1, 2, WorldHolder.instance.randomFillPercent, false, true, true, null, 2, 1); // 0 1; 1 1
 
-			map.GenerateDungeonRegion(1, 0, 35, false, true, false, new[] { 344 }); // 0 0
-			map.GenerateDungeonRegion(2, 0, 35, false, true, true, new[] { 344 }); // 0 0
+			map.GenerateDungeonRegion(1, 0, WorldHolder.instance.randomFillPercent, false, true, false, null); // 0 0
+			map.GenerateDungeonRegion(2, 0, WorldHolder.instance.randomFillPercent, false, true, true, null); // 0 0
+
+			map.GenerateDungeonRegion(0, 3, WorldHolder.instance.randomFillPercent, false, true, true, null, 3, 1); // 0 0
+
+			map.GenerateDungeonRegion(3, 2, WorldHolder.instance.randomFillPercent, false, true, true, null, 1, 1); // 0 0
 		}
 
 		public override void SpawnMonsters()
@@ -39,12 +43,12 @@ namespace Assets.scripts.Mono.MapGenerator.Levels
 
 		public override int GetRegionWidth()
 		{
-			return 50;
+			return 75;
 		}
 
 		public override int GetRegionHeight()
 		{
-			return 50;
+			return 75;
 		}
 
 		public override int GetMaxRegions()
